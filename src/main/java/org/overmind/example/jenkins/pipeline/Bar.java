715e46440d046833b35473bc8c9899132b20c0fc
Eugene Karanda <eugene.karanda@gmail.com>
2017-10-01 02:49:08 +0300

    Initial commit

---
+++ src/main/java/org/overmind/example/jenkins/pipeline/Bar.java
@@ -0,0 +1,11 @@
+package org.overmind.example.jenkins.pipeline;
+
+/**
+ * @author eugene.karanda
+ * @version 1.0 Create: 01.10.2017 2:44
+ */
+public class Bar {
+    public String foo() {
+        return "Hello Jenkins";
+    }
+}
